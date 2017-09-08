c9f11abeec985df12334dbee7093e57e146ffec1
Margaret Berry <margaretshelaghmcgovern@gmail.com>
2017-09-08 09:37:21 -0700

    check to see if the first letter in word 1 matches any letters in word 2

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,8 +4,8 @@ class Words
   #   @word_2 = word_2
   # end
 
-  def anagrams(word_1)
-    word_1.split("")
+  def anagrams(word_1, word_2)
+    word_1_letters = word_1.split("")
+    word_2.match?(word_1_letters[0])
   end
-
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,7 +5,11 @@ require ('anagrams')
 user_words = Words.new()
 
 describe('#anagrams') do
-  it('will split a word into letters') do
-    expect(user_words.anagrams('on')).to(eq(['o','n']))
+  # it('will split a word into letters') do
+  #   expect(user_words.anagrams('on')).to(eq(['o','n']))
+  # end
+
+  it('will see if the first letter of word_1 matches any letters in word_2') do
+    expect(user_words.anagrams('on','jumbotron')).to(eq(true))
   end
 end
