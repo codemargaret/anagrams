19e1141e069454a70dd17602b8fc57ef3c425b7b
Margaret Berry <margaretshelaghmcgovern@gmail.com>
2017-09-08 16:56:57 -0700

    wrap in large function to remove redundant variables, but tests no longer pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -6,92 +6,80 @@ class Words
     @input_2 = input_2
   end
 
-  def is_word(input_1,input_2)
+  def word_cases(input_1,input_2)
     sm_input_1 = input_1.downcase
     sm_input_2 = input_2.downcase
-    input_1_words = sm_input_1.split(" ")
-    input_2_words = sm_input_2.split(" ")
-    words_from_both = input_1_words.concat(input_2_words)
+    no_punc_1 = sm_input_1.gsub(/\W+/, '')
+    no_punc_2 = sm_input_2.gsub(/\W+/, '')
+    input_1_letters = no_punc_1.split("")
+    input_2_letters = no_punc_2.split("")
+
     real_words = []
     not_words = []
+    letter_matches = []
+    trues = []
 
-    words_from_both.each do |word|
-      if (word.include?("a")) || (word.include?("e")) || (word.include?("i")) || (word.include?("o")) || (word.include?("u")) || (word.include?("y"))
-        real_words.push(word)
+    def is_word(sm_input_1,sm_input_2)
+      input_1_words = sm_input_1.split(" ")
+      input_2_words = sm_input_2.split(" ")
+      words_from_both = input_1_words.concat(input_2_words)
+
+      words_from_both.each do |word|
+        if (word.include?("a")) || (word.include?("e")) || (word.include?("i")) || (word.include?("o")) || (word.include?("u")) || (word.include?("y"))
+          real_words.push(word)
+        else
+          not_words.push(word)
+        end
+      end
+
+      if not_words.any?
+        "You need to input actual words!"
+      end
+    end
+
+    def antigrams(input_1_letters,input_2_letters)
+      input_1_letters.each() do |letter|
+        letter_matches.push(no_punc_2.match?(letter))
+      end
+
+      input_2_letters.each() do |letter|
+        letter_matches.push(no_punc_1.match?(letter))
+      end
+
+      letter_matches.each() do |true_false|
+        true_false_str = true_false.to_s
+        if (true_false_str == "true")
+          trues.push(true_false_str)
+        end
+      end
+
+      if trues.empty?
+        "These words have no letter matches and are antigrams!"
+      end
+    end
+
+    def palindromes(no_punc_1,no_punc_2)
+      input_2_rev = no_punc_2.reverse
+
+      if (no_punc_1 == input_2_rev)
+        "These words are palindromes"
+      end
+    end
+
+    def anagrams(input_1_letters,input_2_letters)
+      input_1_letters.each() do |letter|
+        letter_matches.push(no_punc_2.match?(letter))
+      end
+
+      input_2_letters.each() do |letter|
+        letter_matches.push(no_punc_1.match?(letter))
+      end
+
+      if (letter_matches.include?(false))
+        "These words are not anagrams"
       else
-        not_words.push(word)
+        "These words are anagrams"
       end
-    end
-
-    if not_words.any?
-      "You need to input actual words!"
-    end
-  end
-
-  def antigrams(input_1,input_2)
-    sm_input_1 = input_1.downcase
-    sm_input_2 = input_2.downcase
-    no_punc_1 = sm_input_1.gsub(/\W+/, '')
-    no_punc_2 = sm_input_2.gsub(/\W+/, '')
-    input_1_letters = no_punc_1.split("")
-    input_2_letters = no_punc_2.split("")
-    letter_matches = []
-    trues = []
-
-    input_1_letters.each() do |letter|
-      letter_matches.push(no_punc_2.match?(letter))
-    end
-
-    input_2_letters.each() do |letter|
-      letter_matches.push(no_punc_1.match?(letter))
-    end
-
-    letter_matches.each() do |true_false|
-      true_false_str = true_false.to_s
-      if (true_false_str == "true")
-        trues.push(true_false_str)
-      end
-    end
-
-    if trues.empty?
-      "These words have no letter matches and are antigrams!"
-    end
-  end
-
-  def palindromes(input_1,input_2)
-    sm_input_1 = input_1.downcase
-    sm_input_2 = input_2.downcase
-    no_punc_1 = sm_input_1.gsub(/\W+/, '')
-    no_punc_2 = sm_input_2.gsub(/\W+/, '')
-    input_2_rev = no_punc_2.reverse
-
-    if (no_punc_1 == input_2_rev)
-      "These words are palindromes"
-    end
-  end
-
-  def anagrams(input_1, input_2)
-    sm_input_1 = input_1.downcase
-    sm_input_2 = input_2.downcase
-    no_punc_1 = sm_input_1.gsub(/\W+/, '')
-    no_punc_2 = sm_input_2.gsub(/\W+/, '')
-    input_1_letters = no_punc_1.split("")
-    input_2_letters = no_punc_2.split("")
-    letter_matches = []
-    trues = []
-
-    input_1_letters.each() do |letter|
-      letter_matches.push(no_punc_2.match?(letter))
-    end
-
-    input_2_letters.each() do |letter|
-      letter_matches.push(no_punc_1.match?(letter))
-    end
-
-    if (letter_matches.include?(false))
-      "These words are not anagrams"
-    else
-      "These words are anagrams"
     end
   end
 end
