7a6f83dd35a050022292f4c504cd6cdfdcfd2e00
Margaret Berry <codeberry1@gmail.com>
2017-12-16 22:54:27 -0800

    redo tests to use refactored methods with no arguments

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,38 +2,44 @@ require ('rspec')
 require ('pry')
 require ('anagrams')
 
-user_words = Anagram.new(@input_1,@input_2)
-
 describe('#anagrams') do
   it('will tell you if two words are anagrams') do
-    expect(user_words.anagrams('ruby','bury')).to(eq("These words are anagrams"))
+    user_words = Anagram.new('ruby','bury')
+    expect(user_words.anagrams()).to(eq("These words are anagrams"))
   end
 
   it('will tell you if two words are not anagrams') do
-    expect(user_words.anagrams('on','jumbotron')).to(eq("These words are not anagrams"))
+    user_words = Anagram.new('on','jumbotron')
+    expect(user_words.anagrams()).to(eq("These words are not anagrams"))
   end
 
   it('will ignore capital letters') do
-    expect(user_words.anagrams('ruBy','bUry')).to(eq("These words are anagrams"))
+    user_words = Anagram.new('ruBy','bUry')
+    expect(user_words.anagrams()).to(eq("These words are anagrams"))
   end
 
   it('will ignore spaces') do
-    expect(user_words.anagrams('video game','give a demo')).to(eq("These words are anagrams"))
+    user_words = Anagram.new('video game','give a demo')
+    expect(user_words.anagrams()).to(eq("These words are anagrams"))
   end
 
   it('will check for palindromes') do
-    expect(user_words.palindromes('avid','diva')).to(eq("These words are palindromes"))
+    user_words = Anagram.new('avid','diva')
+    expect(user_words.palindromes()).to(eq("These words are palindromes"))
   end
 
   it('will require words with vowels') do
-    expect(user_words.is_word('why hmph', 'she is an unbelievable diva')).to(eq("You need to input actual words!"))
+    user_words = Anagram.new('why hmph', 'she is an unbelievable diva')
+    expect(user_words.is_word()).to(eq("You need to input actual words!"))
   end
 
   it('will check for antigrams') do
-    expect(user_words.antigrams('hi', 'bye')).to(eq("These words have no letter matches and are antigrams!"))
+    user_words = Anagram.new('hi', 'bye')
+    expect(user_words.antigrams()).to(eq("These words have no letter matches and are antigrams!"))
   end
 
   it('will ignore punctuation') do
-    expect(user_words.anagrams("Anti-democratic time table", "Dictator came in, bet I'm late!")).to(eq("These words are anagrams"))
+    user_words = Anagram.new("Anti-democratic time table", "Dictator came in, bet I'm late!")
+    expect(user_words.anagrams()).to(eq("These words are anagrams"))
   end
 end
